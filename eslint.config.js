d1f14d8078dd1780632537d8c576f7c9f59e112a
Hamid Yaftian <hamid.yaftian@gmail.com>
2025-08-14 08:35:18 +0330

    chore: update linters and configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import config from 'eslint-config-reactify';
-import jestDom from 'eslint-plugin-jest-dom';
 import storybook from 'eslint-plugin-storybook';
 
 // eslint-disable-next-line tsdoc/syntax
@@ -7,8 +6,4 @@ import storybook from 'eslint-plugin-storybook';
 export default [
     ...config,
     ...storybook.configs['flat/recommended'],
-    {
-        files: ['**/*.test.{js,jsx,ts,tsx}'],
-        ...jestDom.configs['flat/recommended'],
-    },
 ];
